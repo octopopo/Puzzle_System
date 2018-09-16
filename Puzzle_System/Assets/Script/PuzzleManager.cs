eab0ed96310973f3f0d9e18a7e1a1b32247dbbeb
octopopo <godpopo34@gmail.com>
2018-09-16 15:35:02 -0700

    Create Puzzle Manager
    Create the puzzle manager

---
+++ Puzzle_System/Assets/Script/PuzzleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PuzzleSystem.PuzzlePiece;
+using UnityEngine;
+
+namespace PuzzleSystem.PuzzleManager.V1
+{
+    public class PuzzleManager : MonoBehaviour
+    {
+        
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
