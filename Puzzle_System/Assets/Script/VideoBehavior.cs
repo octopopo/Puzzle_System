646223c2798e05f8be0c6c817836aade9febba8d
octopopo <godpopo34@gmail.com>
2018-10-31 00:45:47 -0700

    Video plaing on an UI
    Allow Video to be played on an UI object

---
+++ Puzzle_System/Assets/Script/VideoBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine;
+
+public class VideoBehavior : MonoBehaviour {
+    public RawImage _rawImage;
+    public VideoPlayer _videoPlayer;
+
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(PlayVideo());
+
+    }
+
+    // Update is called once per frame
+    IEnumerator PlayVideo()
+    {
+        _videoPlayer.Prepare();
+        while(!_videoPlayer.isPrepared)
+        {
+            yield return null;
+        }
+        _rawImage.texture = _videoPlayer.texture;
+        _videoPlayer.Play();
+    }
+}
